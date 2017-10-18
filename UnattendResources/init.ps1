2313842deb40c501b727798194f183f5db9f1350
zhangjl <zhangjl@awcloud.com>
2017-10-18 09:27:50 +0800

    Add delete self deleting operations for local scripts in cloudbase-init
    Change-Id: I5f1ee6035878663a738d6e328cc2664abdb8fcf9
    Reviewed-on: http://gerrit.corp.awcloud.com/8482
    Reviewed-by: zhangjl <zhangjl@awcloud.com>
    Tested-by: zhangjl <zhangjl@awcloud.com>

--- UnattendResources/init.ps1
+++ UnattendResources/init.ps1
@@ -4,3 +4,4 @@ $TelegrafConfPath = "C:\Program Files (x86)\AWC\telegrafa\telegraf_windows.conf"
 (Get-Content $TelegrafConfPath | out-string).Replace("saas_node_id", $instanceid) | Set-Content $TelegrafConfPath
 (Get-Content $TelegrafConfPath | out-string).Replace("metric_storage_service_ip", $metric_storage_service_ip) | Set-Content $TelegrafConfPath
 Restart-Service TelegrafA
+Remove-Item $MyInvocation.MyCommand.Path -Force
